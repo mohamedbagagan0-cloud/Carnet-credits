55b180710f43f463a0631f13cc90a1b6cc4a525e
mohamedbagagan0-cloud <mohamedbagagan0@gmail.com>
2026-09-20 10:12:21 +0000

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,30 @@
+// Change le numéro de version à chaque mise à jour de l'application
+const CACHE = 'carnet-v1';
+const FILES = ['./', 'index.html', 'manifest.json', 'icon-192.png', 'icon-512.png'];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request)
+      .then((res) => {
+        if (res && res.ok && new URL(e.request.url).origin === location.origin) {
+          const copy = res.clone();
+          caches.open(CACHE).then((c) => c.put(e.request, copy));
+        }
+        return res;
+      })
+      .catch(() => caches.match(e.request).then((r) => r || caches.match('index.html')))
+  );
+});
